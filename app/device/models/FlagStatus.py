7c1fe8df5e8fe08c7c8d710b6a32b2b22837ecf9
Josep Escobar <josepescobarn@gmail.com>
2022-03-07 18:17:20 +0000

    Added flag status dictionary

--- app/device/models/FlagStatus.py
+++ app/device/models/FlagStatus.py
@@ -1,37 +1,41 @@
 import json
 import config.default as config
 
+
+
 class FlagStatus():
-    labelsDictionary = {
-        "a": "buzzer",
-        "b": "overload-bypass-function",
-        "j": "power-saving",
-        "k": "display-escape-to-default-automaticaly",
-        "u": "overload-restart",
-        "v": "over-temperature-restart",
-        "x": "backlight-on",
-        "y": "alarm-on-when-primary-source-interrupt",
-        "z": "fault-code-record"
-    }
-    
+    keysDictionary = {
+            "a": "is-buzzer-enabled",
+            "b": "is-overload-bypass-function-enabled",
+            "j": "is-power-saving-enabled",
+            "k": "is-display-escape-to-default-automaticaly-enabled",
+            "u": "is-overload-restart-enabled",
+            "v": "is-over-temperature-restart-enabled",
+            "x": "is-backlight-on-enabled",
+            "y": "is-alarm-on-when-primary-source-interrupt-enabled",
+            "z": "is-fault-code-record-enabled"
+        }
+    flagStatusDictionary = {}
+
     def __init__(self, value): 
         # Sacar la información que tenemos des de la E hasta la D y tendremos los valores activos
-        # EuvxyDabjkzj
         splitedValues = value.split("D")
-        # [0]Euvxy [1]abjkzj
         splitedValues[0] = splitedValues[0].replace("E", "")
         splitedValues[1] = splitedValues[1].replace("\r", "")
         splitedValues[1] = splitedValues[1].replace("+", "")
-        # [0]uvxy [1]abjkzj
-        #['uvxy', 'abjkzj+\r']
 
+        enabledFlags = splitedValues[0]
+        disabledFlags = splitedValues[1]
 
+        # Handle enabled values
+        for index, value in enumerate(enabledFlags):
+            self.flagStatusDictionary[self.keysDictionary[value]] = True
 
-        # El resto hasta el + serán los valores iactivos
-        # Mapear letras con el diccionario y ponerlo todo en un array
-        # Devolverlo codificado en JSON
+        for index, value in enumerate(disabledFlags):
+            self.flagStatusDictionary[self.keysDictionary[value]] = False
+            
         print("🌵 FlagStatus values:")
-        print(splitedValues)
+        print(self.flagStatusDictionary)
 
     def __repr__(self):
         return f'Status({self.gridVoltatge})'
@@ -40,4 +44,4 @@ class FlagStatus():
         return f'{self.gridVoltatge}'
 
     def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+        return json.dumps(self.flagStatusDictionary, default=lambda o: o.__dict__, sort_keys=True, indent=4)
